80a9348dae0e9e2a1d996d092c3fffdd625208a9
Cody Gillespie <cgillespie@pyramidacceptors.com>
2023-10-22 00:43:56 -0700

    Return agent from training

--- train.py
+++ train.py
@@ -6,9 +6,11 @@ import random
 from deep import DQN
 from game import GameState
 
+DEVICE: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 Experience = namedtuple('Experience', ('state', 'action', 'reward', 'next_state', 'done'))
 class ReplayMemory:
-    def __init__(self, capacity): self.memory = deque([], maxlen=capacity)
+    def __init__(self, capacity): self.memory: deque['Experience'] = deque([], maxlen=capacity)
     def push(self, *args): self.memory.append(Experience(*args))
     def sample(self, batch_size): return random.sample(self.memory, batch_size)
     def __len__(self): return len(self.memory)
@@ -16,8 +18,8 @@ class ReplayMemory:
 class DQNAgent:
     def __init__(self, input_dim, action_dim, learning_rate=1e-3):
         # Allow device to be changed to 'cpu' for testing
-        self.q_network = DQN(input_dim, action_dim).to('cuda')
-        self.target_network = DQN(input_dim, action_dim).to('cuda')
+        self.q_network = DQN(input_dim, action_dim).to(DEVICE)
+        self.target_network = DQN(input_dim, action_dim).to(DEVICE)
         self.target_network.load_state_dict(self.q_network.state_dict())
         self.target_network.eval() # set target network to evaluation mode
         self.optimizer = optim.Adam(self.q_network.parameters(), lr=learning_rate)
@@ -26,8 +28,7 @@ class DQNAgent:
         self.batch_size = 64
 
     def select_action(self, state, epsilon):
-        if random.random() < epsilon: # epsilon-greedy exploration
-            return random.randrange(4)
+        if random.random() < epsilon: return random.randrange(4)
         else:
             with torch.no_grad():
                 return self.q_network(state).max(1)[1].item()
@@ -38,7 +39,7 @@ class DQNAgent:
         batch = Experience(*zip(*transitions))
 
         state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
+        action_batch = torch.cat((torch.tensor(batch.action, dtype=torch.int64).unsqueeze(1).to(DEVICE),))
         reward_batch = torch.cat(batch.reward)
         next_state_batch = torch.cat(batch.next_state)
         done_batch = torch.cat(batch.done)
@@ -56,7 +57,7 @@ class DQNAgent:
     def update_target_network(self):
         self.target_network.load_state_dict(self.q_network.state_dict())
 
-def train():
+def train() -> DQNAgent:
 
     episodes = 1000
     epsilon_start = 1.0
@@ -68,16 +69,16 @@ def train():
 
     for episode in range(episodes):
         game = GameState.new() # Start a new game
-        state = torch.tensor(game.cells, dtype=torch.float32).unsqueeze(0).to('cuda')
+        state = torch.tensor(game.cells, dtype=torch.float32).unsqueeze(0).to(DEVICE)
         done = False
         while not done:
             action = agent.select_action(state, epsilon)
             next_state = { 0: game.up(), 1: game.down(), 2: game.left(), 3: game.right() }[action]
             reward = next_state.score - game.score
             done = next_state.is_game_over()
-            next_state = torch.tensor(next_state.cells, dtype=torch.float32).unsqueeze(0).to('cuda')
-            reward = torch.tensor([reward], dtype=torch.float32).to('cuda')
-            done = torch.tensor([done], dtype=torch.uint8).to('cuda')
+            next_state = torch.tensor(next_state.cells, dtype=torch.float32).unsqueeze(0).to(DEVICE)
+            reward = torch.tensor([reward], dtype=torch.float32).to(DEVICE)
+            done = torch.tensor([done], dtype=torch.uint8).to(DEVICE)
             
             agent.memory.push(state, action, reward, next_state, done)
             state = next_state
@@ -89,3 +90,4 @@ def train():
         
         epsilon = max(epsilon_end, epsilon_decay * epsilon) # decay epsilon
 
+    return agent
